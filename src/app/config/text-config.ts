ecd992d49c8723ce6f4e62f43ddc7f62abd1e155
apocca <guidoromano1989@gmail.com>
2024-05-18 17:16:24 -0300

    Terminada

--- src/app/config/text-config.ts
+++ src/app/config/text-config.ts
@@ -134,37 +134,220 @@ export const TextConfig = {
 {
   id: 16,
   articleHeading: 'Titulo 6',
-  articleIntro: 'Sexto texto',
-  articleMoreText: 'Sexto texto oculto.',
+  articleIntro: '"Ballroom" es la denominación que se usa para designar un conjunto de 10 ritmos. Pertenecen a los llamados ritmos "Standar" el Vals Inglés, Vals Vienés, Tango, Foxtrot, Quickstep.',
+  articleMoreText: 'Corresponden a los ritmos "Latin" Samba, Cha Cha Cha, Rumba, Paso Doble, Jive. En una competencia se puede competir en los 10 ritmos abarcando Standar y Latin, o en los 5 ritmos de Standar, o en los 5 ritmos de Latin. Pero nunca en 1 solo ritmo a selección de alguna modalidad. Se compite siempre o en los 10 ritmos juntos, o los 5 de alguna de las dos modalidades por separados.',
   imageUrl: '',
   articleLink: ''
 },
+
 {
   id: 17,
   articleHeading: 'Titulo 7',
-  articleIntro: 'Séptimo texto',
-  articleMoreText: 'Séptimo texto oculto.',
+  articleIntro: 'Y sí, cuando pisas la pista de baile tenés que forzar la postura, no mirar el piso, esforzarte, tratar de sobresalir...',
+  articleMoreText: 'Como dice mi entrenador cuando nos prepara para bailar en los torneos: "Ustedes en la pista no son personas, son bailarines"',
   imageUrl: '',
   articleLink: ''
 },
 {
   id: 18,
   articleHeading: 'Titulo 8',
-  articleIntro: 'Octavo texto',
-  articleMoreText: 'Octavo texto oculto.',
+  articleIntro: "Un gran 'SÍ' implica muchos pequeños 'No'. Decir 'SÍ' a bailar, a realmente aprender y formarse como un bailarín, conlleva sacrificios diarios que a menudo pasan desapercibidos.",
+  articleMoreText: "Bailar bien no es resultado de un curso rápido que promete habilidades en 30 días con una garantía de empleo. No. Bailar bien requiere, como mínimo, meses de dedicación y estudio profundo. Implica asistir regularmente a clases, dejar atrás vicios que podrían menguar nuestro rendimiento físico, y renunciar a las constantes salidas de fiesta que podrían afectar nuestra recuperación. Implica regresar a casa después de horas en el salón y seguir estudiando.\n\nAprender a lidiar con la incertidumbre de eventos, la aceptación del público, y la superación de las expectativas. Implica NO adoptar malos hábitos alimenticios. Implica empezar a renunciar a las salidas frecuentes con amigos o familia. La selectividad en el tiempo compartido se convierte en una realidad. No derrochar en gastos innecesarios, pues el mundo del arte demanda inversiones considerables con retornos a largo plazo. No trasnochar frecuentemente (siempre que sea posible). Significa practicar hasta altas horas de la noche, madrugar al día siguiente para seguir practicando, y vivir con la incertidumbre del resultado. Esa es la única verdad.\n\nPero veámoslo desde el lado positivo, por más desafiante que sea la danza y sin importar las circunstancias, SIEMPRE hay espacio para mejorar y aprender. Solo depende de la práctica, la voluntad y la disciplina.",
   imageUrl: '',
   articleLink: ''
-},
+}
+,
 {
   id: 19,
   articleHeading: 'Titulo 9',
-  articleIntro: 'Noveno texto',
-  articleMoreText: 'Noveno texto oculto.',
+  articleIntro: 'Nadie nace siendo buen bailarín/na, o bueno bailando.\n\nEn danza es casi todo, destreza adquirida.\n\nAsí como también, no hay una sola forma de ser buen bailarín.',
+  articleMoreText: 'El secreto de bailar bien: Escuchar a tus maestros y luego, encerrarte en tu pieza a practicar hasta el quiebre emocional o que ya no puedas más, o júntate con amig@s que estén igual de emocionados que vos y pásensela genial en esos ensayos.\n\nY algún día, todo el mundo los va a aplaudir, van a terminar diciendo "¡Wuaw! ¡Yo quiero bailar como él!',
   imageUrl: '',
   articleLink: ''
 },
 {
   id: 20,
+  articleHeading: 'Titulo 10',
+  articleIntro: 'Me honran todas y cada una de las personas que me eligen para que les enseñe a bailar.',
+  articleMoreText: 'Así que prometo nunca cerrar la puerta a un estudiante que llegue tarde, ni retirar del salón a quien no tenga la vestimenta "de primera línea" para entrenar, ni mucho menos a quien llegue con los zapatos de práctica rotos (porque conozco de sobra los grandes sacrificios que hay que hacer para bailar).\n\nMi única meta en este nuevo camino, es encender esa chispa, es que logren sacar o descubran ese bailarín/na que llevan dentro, ese amor por la danza, tal cual lo hicieron mis maestros conmigo.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 21,
+  articleHeading: ' Hay que tener en cuenta la comodidad del vestuario para prácticar o competir:',
+  articleIntro: '• Para practicar tener en cuenta la comodidad combinado con la conciencia que el profesor o profesora, deberá corregirte líneas, postura y el uso correcto de los músculos. El vestuario podría facilitar este trabajo.',
+  articleMoreText: 'Para competir hay que tener en cuenta: el estilo de tu danza, tus intenciones, el vestuario de tu pareja, la elegancia, tus particularidades acerca de tu somatotipo, los reglamentos vigentes de la asociación de baile deportivo donde participarás.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 22,
+  articleHeading: '¿Qué zapatos debo usar para Ballroom Dance?',
+  articleIntro: 'Si bien no es necesario tener el calzado específico de Ballroom Dance en tus primeras clases, con el transcurrir del tiempo se vuelven imprescindibles para que la práctica sea la adecuada.',
+  articleMoreText: 'Se debe a la costumbre que le darás a tus músculos de pisar de forma correcta con el calzado específico.',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 23,
+  articleHeading: ' ¿Quieres mantenerte al tanto de todas las novedades y eventos exclusivos de las clases de BASC?',
+  articleIntro: '¡Estamos emocionados de anunciar que hemos creado un grupo de WhatsApp para ti! ',
+  articleMoreText: 'Únete ahora para recibir actualizaciones instantáneas, detalles de las clases y mucho más. No te pierdas esta oportunidad de ser parte de nuestra vibrante comunidad de bailarines.\n\n¡Únete hoy mismo haciendo clic en el enlace de abajo y déjate llevar por el ritmo! 🔗:',
+  imageUrl: '',
+  articleLink: 'https://chat.whatsapp.com/IK5BVsSuMQ4LHX6Nc6cDAj'
+},
+{
+  id: 24,
+  articleHeading: 'Cosas que puedes hacer para apoyar a tu hijo/a que quiere ser bailarín',
+  articleIntro: 'Si la danza es un ambiente ajeno a tu experiencia, ten en cuenta estos tips que siempre puedes hacer para acompañar a crecer a tu hijo/a en este mundo:',
+  articleMoreText: '1. Intenta ir a verle siempre que tenga presentaciones, shows, competencias, etc.\n\nEntiende, escuchale, y apoya sus sueños. La danza es una hermosa vida, pero requiere trabajo y paciencia.\n\nAyúdalo/a con su peinado, vestuario, maquillaje.\n\nGrita su nombre y aplaude después de cada presentación.\n\nAcompáñalo/a a sus ensayos y clases de danza.',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 25,
+  articleHeading: 'Permítanme presentarme, soy Juan Manuel, el instructor a cargo en BASC.',
+  articleIntro: 'Mi historia en el mundo del baile comenzó con una chispa de inspiración al presenciar la gracia y la destreza del bailarín Oleksandr Honcharov, fue él lo que me llevó a querer aprender esta danza.',
+  articleMoreText: 'Mi búsqueda de conocimiento me hizó descubrir DSC, una escuela de baile que se convertiría en el punto de partida de mi viaje en este apasionante universo. Recuerdo el día que crucé por primera vez las puertas de la sucursal en Avenida Corrientes, y conocí a los dos maestros que dejaron una huella indeleble en mi formación: Mery Lenz y Eugenio Dmytrenko. Desde ese momento, supe que quería absorber cada enseñanza que estos virtuosos de la danza tenían para ofrecerme.\n\nA lo largo de los años, he participado en competiciones de ballroom, destacándome en categorías de letras y Open, obteniendo primeros puestos y llegando a ser finalista en múltiples ocasiones. He compartido mis conocimientos a través de clases grupales y particulares en diversas provincias de Argentina, desde Buenos Aires hasta Santa Fe, desde niveles iniciales hasta avanzados.\n\nSin embargo, el año 2024 marca un nuevo capítulo en mi trayectoria, ya que he decidido emprender mi propio proyecto personal: BASC. Mi objetivo con BASC es claro: difundir los bailes de salón, transmitir mi amor por esta danza y apoyar, formar y acompañar a todos aquellos que deseen adentrarse en este apasionante mundo.\n\nPor lo tanto, los invito a todos a unirse a mí en este viaje de aprendizaje y descubrimiento. ¡Los espero con los brazos abiertos para aprender a bailar y compartir juntos este emocionante camino!',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 26,
+  articleHeading: ' ¡Bienvenido a BASC (Ballroom Argentina Sport Club)!',
+  articleIntro: ' Nuestra misión es difundir la pasión por el baile en Argentina y Latinoamérica. Ofrecemos clases para todos los niveles: principiantes, intermedios y avanzados. Ya sea que bailes por hobby, recreación o competitivamente, ¡tenemos algo para ti!',
+  articleMoreText: 'Nos preocupamos por tu progreso, por eso nuestros instructores reciben formación continua en ballroom dance, preparación física y psicología deportiva. En BASC, creamos un ambiente cómodo donde el error es parte del aprendizaje y, sobre todo, ¡donde te diviertes mientras aprendes!\n\n¿Preguntas? ¿Quieres más información? ¡No dudes en contactarnos a través de MD y te responderemos lo antes posible!',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 27,
+  articleHeading: '¿Por qué aconsejo a los hombres aprender la parte coreográfica de las mujeres?',
+  articleIntro: 'Tú serás quien conduce, por lo tanto deberás saber que pasos debe hacer ella.',
+  articleMoreText: 'Aprendar su coreografía y sus figuras, te hará entender que debe sentir ella y como, para que ella pueda desplegarse mejor en la coreografía.',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 28,
+  articleHeading: '¿Cómo ir vestido a tus primeras clases de ballroom?',
+  articleIntro: 'Pantalón o jogging, remera y camisa cómodos\n\nZapatos o zapatillas flexibles',
+  articleMoreText: 'Luego, a medida que el alumno continué asistiendo a clases, se recomienda usar la ropa característica de ballroom, como los pantalones y zapatos.\n\nPuntualmente, el calzado de ballroom se vuelve una herramienta imprescindible para asistir a clases.(El color negro es por tradición el color que más se estila para asistir a clases)',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 29,
+  articleHeading: 'SEÑALES DE SOBRENTRENAMIENTO',
+  articleIntro: '1) Sentir ansiedad prácticamente a diario.\n\n2) Sentirse constantemente fatigado, desmotivado o agotado emocionalmente',
+  articleMoreText: '3) No estar disfrutando nada de tu deporte.\n\n4) Considerar que estás la mayor parte de tu día a día irritable o susceptible.\n\n5) Estar alejándote de tus vínculos sociales.',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 30,
+  articleHeading: 'Qué evalúan en las primeras rondas de los torneos de ballroom?',
+  articleIntro: 'Postura\n\nTécnica básica\n\nPrecisión musical\n\nClaridad de las figuras',
+  articleMoreText: 'Décimo texto oculto.',
+  imageUrl: '',
+  articleLink: ''
+},
+{
+  id: 31,
+  articleHeading: '¿Quién gana un torneo de ballroom?',
+  articleIntro: '¿Los más rápidos?\n\n¿Los más técnicos?\n\n¿Los más precisos?\n\n¿La coreografía más complicada?\n\n¿La mejor conexión?',
+  articleMoreText: '🟡Puede que una suma balanceada de todos estos factores sea una respuesta, incluso puede que esten balanceados en todo esto y sobresalgan en una de estas cualidades, lo que es bueno ya que da la marca personal. Y todos sabemos lo importante que es ser único y original en el arte.\n\nBryan Watson dice que el mejor bailarín es quien mejor expresa la música. La técnica, las herramientas y habilidades son herrramientras para ello, pero no replicar constantemente la coreografía una y otra vez, ejecutando la mécanica de los movimientos a la perfección, infinitas veces en todos los temas de todos los ritmos de la misma manera, eso no te hará un gran bailarín. Debes adecuar tus herramientas para expresar de la mejor forma posible la música.\n\nPor lo tanto, la coreografía con distintas canciones debe verse diferente (musicalmente por lo menos). Si se ven todas iguales, eres como un atleta de 100 mt llanos, que apenas suena la campana sale corriendo y dando lo máximo físicamente que puede entregar su cuerpo. Perfecto, eres un gran atleta, pero no estás realmente bailando. Y digo que no estás realmente bailando porque no hay una escucha, una interpretación y una expresión de lo que la música te genera. Y aquí en danza hay que tener en cuenta que también y en gran parte es arte, y como dijo Bruce, el arte no solo es embellecimiento u ornamento, también es expresión.\n\n¿Y tú que pareja crees que debe ganar una competencia?',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 32,
+  articleHeading: 'Bailarines de Showmatch vs Bailarines de Ballroom',
+  articleIntro: 'A raíz de la aparición del ballroom en televisión de la mano de programas como "Showmatch", "Strictly Come Dancing" o "Dancing Whit the Stars", el ballrom ha logrado tener cierta llegada al público. Sin embargo, es importante distinguir las diferencias entre el ballroom original y la fusión de elementos que se ven en los show televisivos.',
+  articleMoreText: '1) COREOGRAFÍAS: Las coreografías son sin duda el principal elemento distintivo entre los concursos televisivos y el ballroom original.\n\nLas coreografías hechas para los programas, tienen como finalidad principal el show y el espectáculo. Las coreografías de ballroom tiene como directriz subyacente el concepto de "elegancia", lo que suele derivar en prescindir de trucos y acrobacias.\n\nCONTEXTO // AMBIENTE: En los concursos televisivos se puede preparar todo un show alrededor de una coreografía. Se permite tanto elegir el tema y la comodidad de bailar una sola pareja en la pista, sumado a que no se encuentran los parámetros de evaluación por los cuales son evaluadas las parejas en competición.\n\nEn los torneos de ballroom, se suele bailar con varias parejas al mismo tiempo, no se puede elegir la canción o música. Si bien hay un orden de ritmos predeterminados (Samba, Cha Cha Cha, Rumba, Paso Doble, Jive) no se puede elegir el tema a bailar. Simplemente la música suena, los bailarines demuestran su capacidad de interpretación, escucha de tiempos y bailan.\n\nEs mucho menos controlado, las parejas tienen coreografías acorde a este factor y también teniendo en cuenta que son evaluadas por: conexión física, coreografía, postura y balance, calidad de los movimientos, musicalidad, precisión, fluidez en las transiciones. Pero no buscan el puro show o llamar la atención, de hecho, muchas veces se prescinden de esos factores porque rompe con la estética de los torneos, en otras palabras, "ensucian" la expresión de la música.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 33,
+  articleHeading: '3 Puntos claves para todo practicante inicial de ballroom',
+  articleIntro: 'En las etapas iniciales de Ballroom Latin, donde un bailarín/na empieza con su alfabetización en cuanto al nombre de las figuras, los tiempos de cada ritmo, los fundamentos del ballroom, conviene tener en cuenta los siguientes conceptos:',
+  articleMoreText: '1. Figuras claras: Debe quedar claro las distinciones entre una figura, la transición, y la figura que le continua.\n\n2. Tiempos precisos: Criterio fundamental y crítico tanto a la hora de evaluar una pareja, como también el poder demostrar que se entiende el lenguaje por el que esta compuesto la música.\n\n3. Postura correcta: Fundamental tanto a la hora de la presentación del bailarín tanto individual como la pareja, ya que transmite entre tantas cosas, la presencia hacia el público, la claridad de los movimientos, la distinción y caracterización de cada ritmo.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+
+{
+  id: 34,
+  articleHeading: '¿Por qué recomiendo asistir a clases de baile social a todo competidor de ballroom? ',
+  articleIntro: 'Por un motivo muy puntual: porque irás a bailar por el simple hecho de bailar. PUNTO.',
+  articleMoreText: 'En ballroom se enfocan tanto en la competición y en maximizar el rendimiento deportivo, que se terminan olvidando de algo fundamental: el placer de bailar por bailar.\n\nY bueno... es entendible. Si en todas las clases todo lo que se enseña va enfocado en competir y ganar, se te juzga constantemente, es un caldo de cultivo perfecto para padecer problemas de ansiedad generados por el ambiente de la danza deportiva.\n\nEntonces en danzas del tipo social (salsa, tango, bachata por ejemplo) se baila por el simple motivo de disfrutar bailando. Incluso esto tendrá un beneficio para el ámbito competitivo: te hará recordar lo que es sentir la música sin tanta estructura previa de como sentirla y moverse, y también, disfrutar bailar sin más, lo cual se disfruta muchísimo más que competir e intentar ser mejor que los demás, y eso se transmitirá hacia los demás. Y por ende, tu danza será maravillosa.\n\nY de paso vamos a salsear juntos 😉.\n\nOff topic: bailar no es correr, además, uno cuando no se encuentra en modo competitivo es cuando más creativo se vuelve.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 35,
+  articleHeading: 'Detrás de un buen bailarín SIEMPRE hay un gran maestro de danza.',
+  articleIntro: 'Porque inspiran con su pasión por bailar. Son fuente de motivación y el vínculo que realiza un bailarín con su entrenador puede ser tan fuerte como el de un padre con su hijo...',
+  articleMoreText: 'Porque hay entrenadores que se convierten en padres y padres que se vuelven entrenadores.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 36,
+  articleHeading: 'Titulo 10',
+  articleIntro: 'Dando clases, observo que hay gente que es tan buena bailando que sólo tengo que hacer de psicólogo y destrabar los bloqueos para que haga lo que naturalmente su cuerpo sabe hacer.',
+  articleMoreText: 'Hay gente que me canta la musicalidad con el cuerpo, sin escuchar la música ya sé que ritmo estan bailando.\n\nEn varios casos, el bailarín o la bailarina, siempre estuvieron ahí, solo tengo que sacar las cuerdas que le sujetan.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 37,
+  articleHeading: 'Titulo 10',
+  articleIntro: 'La técnica es en danza, la/las herramientas básicas a partir de las cuales desarrollará uno su propia manera de expresarse o ser, adoptando lo útil, desechando lo inútil, y agregando lo esencialmente propio de uno, desarrollando así su propio estilo. Funciona como bálsamo que lo ayude a cruzar de orilla a orilla.',
+  articleMoreText: 'Pero no es un Dios al que reverenciar ciegamente y si no es así, esta mal, no baila.... no seas esclavo de la teoría. Experimenta, prueba por ti mismo.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 38,
+  articleHeading: 'Titulo 10',
+  articleIntro: 'Décimo texto',
+  articleMoreText: 'Décimo texto oculto.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 39,
+  articleHeading: 'Titulo 10',
+  articleIntro: 'Décimo texto',
+  articleMoreText: 'Décimo texto oculto.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 40,
+  articleHeading: 'Titulo 10',
+  articleIntro: 'Décimo texto',
+  articleMoreText: 'Décimo texto oculto.',
+  imageUrl: '',
+  articleLink: ''
+},
+
+{
+  id: 41,
   articleHeading: 'Titulo 10',
   articleIntro: 'Décimo texto',
   articleMoreText: 'Décimo texto oculto.',
