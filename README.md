dc6193137d4afd1929de32ff90e53948318567f4
apocca <guidoromano1989@gmail.com>
2024-05-29 17:46:03 -0300

    formulario listo

--- README.md
+++ README.md
@@ -1,25 +1,4 @@
 ng build
 git add . 
-git commit -m "preparando formulario" 
+git commit -m "formulario listo" 
 git push origin main
-
-
-
-
-
-  { id: 3, 
-    titulo: '¿Qué días son las clases de Ballroom?' ,
-    info: clase[0].dia + ', ' + clase[1].dia + ', ' + clase[2].dia},
-
-  { id: 4,
-    titulo: '¿Cuáles son los horarios de las clases?' ,
-    info: clase[0].dia + ', ' + clase[0].horario + ' / ' + clase[1].dia + ', ' + clase[1].horario + ' / ' + clase[2].dia + ', ' + clase[2].horario},
-
-  { id: 5, 
-    titulo: '¿Dónde se realizan las clases?' ,
-    info:  clase[0].dire + ' / ' + clase[1].dire + ' / ' + clase[2].dire },
-
-  { id: 6,
-    titulo: '¿Cuál es el precio de las clases?' ,
-    info: clase[0].precio + ' / ' + clase[1].precio + ' / ' + clase[2].precio + ' al Mes.'},
-
--- src/app/pages/contact/form/form.component.ts
+++ src/app/pages/contact/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-form',
@@ -10,9 +9,123 @@ import { HttpClient } from '@angular/common/http';
 export class FormComponent {
   form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      from_name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+      to_name: ['Admin', Validators.required],
+      from_mail: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(255)]],
+    });
+  }
+
+  async send() {
+    emailjs.init('1N345HBz7rZCbp8mT');
+    try {
+      let response = await emailjs.send("service_iqnmw9c", "template_lxl7e34", {
+        from_name: this.form.value.from_name,
+        to_name: this.form.value.to_name,
+        from_mail: this.form.value.from_mail,
+        message: this.form.value.message,
+      });
+      alert('Mensaje enviado');
+      this.form.reset();
+    } catch (error) {
+      console.error('Error al enviar el mensaje:', error);
+      alert('Hubo un error al enviar el mensaje');
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.send();
+    } else {
+      console.log('Formulario inválido');
+      this.form.markAllAsTouched();
+      console.log(this.form);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import  emailjs from '@emailjs/browser';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent {
+form: FormGroup = this.fb.group{{
+  from_name: '',
+  to_name: 'Admin',
+  from_mail: '',
+  message: '',
+}};
+
+constructor(private fb: FormBuilder) {}
+
+async send() {
+  emailjs.init('1N345HBz7rZCbp8mT');
+  let response= await emailjs.send("service_iqnmw9c","template_lxl7e34",{
+    from_name: "this.form.value.from_name",
+    to_name: "this.form.value.to_name",
+    from_mail: "this.form.value.from_mail",
+    message: "this.form.value.message",
+    });
+
+    alert('Mensaje enviado')
+    this.form.reset();
+  }
+
+
+
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30), Validators.pattern('[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+')]],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(255)]],
     });
@@ -37,3 +150,28 @@ export class FormComponent {
     }
   }
 }
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
