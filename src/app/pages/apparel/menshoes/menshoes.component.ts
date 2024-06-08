541b66f32c06ca3bc7a623e6ea506f91aec75d5c
apocca <guidoromano1989@gmail.com>
2024-06-08 18:06:45 -0300

    agregando productos

--- src/app/pages/apparel/menshoes/menshoes.component.ts
+++ src/app/pages/apparel/menshoes/menshoes.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-menshoes',
-  templateUrl: './menshoes.component.html',
-  styleUrls: ['./menshoes.component.css']
+  templateUrl: './menshoes.component.html'
 })
-export class MenshoesComponent {
+export class MenshoesComponent  implements OnInit, OnDestroy {
+  menshoes: any[] = [];
+  private langChangeSubscription: Subscription | undefined;
 
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+  }
+
+  ngOnInit(): void {
+    this.loadText();
+    this.langChangeSubscription = this.translate.onLangChange.subscribe(() => {
+      this.loadText();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.langChangeSubscription) {
+      this.langChangeSubscription.unsubscribe();
+    }
+  }
+
+  loadText(): void {
+    this.translate.get('MENSHOES').subscribe((data: any[]) => {
+      this.menshoes = data;
+    });
+  }
 }
+
