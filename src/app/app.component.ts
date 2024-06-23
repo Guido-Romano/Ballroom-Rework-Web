3ac02ec1c79303d45e7bbb303883745d5ab16183
apocca <guidoromano1989@gmail.com>
2024-06-23 17:10:49 -0300

    EN lenguage error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en');
-    const userLang = navigator.language.split('-')[0]; 
-    translate.use(userLang); 
-  }
+  title = 'ballroom-2.0';
 }
 
